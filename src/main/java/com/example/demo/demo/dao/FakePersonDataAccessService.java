4265640aaa45e9c17ef67aca82574a60bdf74309
Will <Will@Users-MacBook-Pro.local>
2020-07-25 10:17:30 +0100

    prepear system to run sprintboot

--- src/main/java/com/example/demo/demo/dao/FakePersonDataAccessService.java
+++ src/main/java/com/example/demo/demo/dao/FakePersonDataAccessService.java
@@ -1,13 +1,14 @@
 package com.example.demo.demo.dao;
 
 import com.example.demo.demo.module.Person;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
+@Repository("fakeDao")
 public class FakePersonDataAccessService implements PersonaDao {
-
     private static List<Person> DB = new ArrayList<>();
 
     @Override
--- src/main/java/com/example/demo/demo/service/PersonService.java
+++ src/main/java/com/example/demo/demo/service/PersonService.java
@@ -2,12 +2,16 @@ package com.example.demo.demo.service;
 
 import com.example.demo.demo.dao.PersonaDao;
 import com.example.demo.demo.module.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
+@Service
 public class PersonService {
-
     private final PersonaDao personaDao;
 
-    public PersonService(PersonaDao personaDao) {
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonaDao personaDao) {
         this.personaDao = personaDao;
     }
 
